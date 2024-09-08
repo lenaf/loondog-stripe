0349b7c2cca1f624b31735509e9f7919e61eca1d
lenaf <lforti41@gmail.com>
2024-09-08 17:46:33 -0400

    fix build

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from './Link'
-import siteData from '@/data/siteData'
 import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
@@ -7,10 +6,10 @@ export default function Footer() {
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteData.email}`} size={6} />
+          {/* <SocialIcon kind="mail" href={`mailto:${siteData.email}`} size={6} /> */}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteData.author}</div>
+          {/* <div>{siteData.author}</div> */}
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
